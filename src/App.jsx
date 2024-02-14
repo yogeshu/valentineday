1f07b31d2eef2d6e9d4506b690767830de5e7a71
Yogesh Bhavsar <32289745+yogeshu@users.noreply.github.com>
2024-02-14 15:17:40 +0530

    Update App.jsx
    Removed Footer

--- src/App.jsx
+++ src/App.jsx
@@ -80,22 +80,9 @@ export default function Page() {
           </div>
         </>
       )}
-      <Footer />
+     
     </div>
   );
 }
 
-const Footer = () => {
-  return (
-    <a
-      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/Xeven777/valentine"
-      target="__blank"
-    >
-      Made with{" "}
-      <span role="img" aria-label="heart">
-        ❤️
-      </span>
-    </a>
-  );
-};
+
